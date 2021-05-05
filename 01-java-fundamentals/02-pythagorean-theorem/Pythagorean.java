a870e0753c9217483cbf7b3f84b50d426063b050
Evan Pebbles <78051545+Epebbles@users.noreply.github.com>
2021-05-05 10:16:25 -0700

    Add files via upload

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,5 @@
+public class Pythagorean {
+        public double calculateHypotenuse (int lenA, int lenB) {
+            return Math.sqrt((lenA * lenA ) + (lenB * lenB));
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean iD = new Pythagorean();
+        double four = 4.0;
+        double squareRoot = Math.sqrt(four);
+        System.out.println(squareRoot);
+    }
+}
